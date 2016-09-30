293cf23e2ae368ae8baa9916e097187a965fdeb2
Bruno Goncalves <bgoncal@its.jnj.com>
2016-09-29 21:51:52 -0300

    Final changes

--- controllers/search.js
+++ controllers/search.js
@@ -24,48 +24,57 @@ function SearchController($cookies, $http, $window, $location, authService, NgMa
     if (cookies) {
     }
     else {
-      $window.location.href ="#/users/login/customers?cep=" + getParameterByName('cep') + "&atuacao=" + getParameterByName('atuacao');
+        $window.location.href = "#/users/login/customers?cep=" + getParameterByName('cep') + "&atuacao=" + getParameterByName('atuacao');
     }
 
     function getStars(number) {
-      number = Math.floor(number);
+        number = Math.floor(number);
         var arr = [];
         for (var i = 0; i < number; i++) {
             arr.push(i);
         }
         return arr;
     }
+
     function getEmptyStars(number) {
-			number = (5 - Math.floor(number));
+        number = (5 - Math.floor(number));
         var arr = [];
         for (var i = 0; i < number; i++) {
             arr.push(i);
         }
         return arr;
-		}
+    }
 
     function openSearch() {
         angular.element("#modalSearch").openModal();
     }
+
     function closeSearch() {
         angular.element("#modalSearch").closeModal();
     }
+
     vm.loading = true;
     var globals = $cookies.getObject('globals');
-    url = helperService.backendUrl + "/relat/pesquisa_completa.php?cep=" + getParameterByName('cep') + "&atuacao=" + getParameterByName('atuacao') + "&pacientesID=" + globals.currentUser.pacientesID;
-    $http.get(url)
-        .then(function(res) {
-            console.log('succeess', res);
-            vm.loading = false;
-            vm.result = res.data;
-            vm.location = res.data[vm.result.length -1];
-            console.log(vm.location);
-            console.log(vm.result);
-        }, function(err) {
-            console.log('error', err);
-            $window.location.reload();
-        });
 
+    if (globals.currentUser.pacientesID != null) {
+        url = helperService.backendUrl + "/relat/pesquisa_completa.php?cep=" + getParameterByName('cep') + "&atuacao=" + getParameterByName('atuacao') + "&pacientesID=" + globals.currentUser.pacientesID;
+
+        $http.get(url)
+            .then(function (res) {
+                console.log('succeess', res);
+                vm.loading = false;
+                vm.result = res.data;
+                vm.location = res.data[vm.result.length - 1];
+                console.log(vm.location);
+                console.log(vm.result);
+            }, function (err) {
+                console.log('error', err);
+            });
+    }
+    else {
+        $window.alert("Desculpe, essa pesquisa só pode ser realizada por um perfil de Paciente Cliente. Obrigado");
+        $window.location.href = "#/";
+    }
 
 
     vm.submitForm = submitForm;
